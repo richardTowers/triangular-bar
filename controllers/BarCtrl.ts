406f8ff7d31a1b9ea2776bdecf296973e1432fcd
Richard Towers <richard@richard-towers.com>
2014-01-04 17:57:16 +0000

    Semicolons

--- controllers/BarCtrl.ts
+++ controllers/BarCtrl.ts
@@ -21,14 +21,14 @@ module triangularBar {
         constructor(
             private $scope: IBarScope
         ) {
-            $scope.graph = { width: 100, height: 100 }
+            $scope.graph = { width: 100, height: 100 };
             $scope.bars = [
                 { height: 100 },
                 { height: 10 },
                 { height: 87 },
                 { height: 42 },
                 { height: 91 }
-            ]
+            ];
         }
     }
 }
