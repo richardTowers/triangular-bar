962e6d9e7e5dac0b63406ce9a38a43f24a04169f
Richard Towers <richard@richard-towers.com>
2014-01-04 15:37:29 +0000

    On to an SVG + Angular Hello World
    Credit to Alexandros Marinos.
    
    http://alexandros.resin.io/angular-d3-svg/

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+/// <reference path="bower_components/DefinitelyTyped/angularjs/angular.d.ts" />
+/// <reference path="bower_components/DefinitelyTyped/d3/d3.d.ts" />
+var app = angular.module('triangular', []);
+
+app.controller('BarCtrl', function ($scope) {
+    $scope.graph = { 'width': 1000, 'height': 1000 };
+    $scope.circles = [
+        { 'x': 150, 'y': 200, 'r': 150, 'fill': '#ff0000' },
+        { 'x': 500, 'y': 700, 'r': 200, 'fill': '#00ff00' },
+        { 'x': 800, 'y': 100, 'r': 100, 'fill': '#0000ff' }
+    ];
+});
 //# sourceMappingURL=app.js.map
